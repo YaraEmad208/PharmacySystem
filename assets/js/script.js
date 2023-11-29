d2101ff433e78546853d4dc8b3bbd210fbfa611f
Yara <104102462+YaraEmad208@users.noreply.github.com>
2023-11-29 02:21:35 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,366 @@
+// this data website 
+
+//some product in home page  and this Medications
+let some_product=[
+  {'image':'./assets/images/best/1-t.jpg',price:'$20','description':'Elbaluran 60Mg 30 Film Coated Tablets Coated '},
+  {'image':'./assets/images/best/2-t.jpg',price:'$10','description':'Elbaluran 40Mg 30 Film Coated Tablets '},
+  {'image':'./assets/images/best/3-t.jpg',price:'$30','description':'Elbaluran 80Mg 30 Film Coated Tablets '},
+  {'image':'./assets/images/best/4-t.jpg',price:'$50','description':'Cymbatex 20Mg 30 Capsules, Conventin 300Mg 30 Capsules '},
+  {'image':'./assets/images/best/5-t.jpg',price:'$5','description':'Apilipex 30Mg 30 Tablets '},
+  {'image':'./assets/images/best/6-t.jpg',price:'$4','description':'Sycocetam 500Mg 20 Tablets'},
+  {'image':'./assets/images/best/7-t.jpg',price:'$15','description':'Citapronex 20Mg 14 Tablets '},
+  {'image':'./assets/images/best/8-t.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+]
+
+// Hair Care
+let Hair_Care=[
+  {'image':'./assets/images/best/hire-1.jpg',price:'$20','description':'Elbaluran 60Mg 30 Film Coated Tablets Coated '},
+  {'image':'./assets/images/best/hire-2.jpg',price:'$10','description':'Elbaluran 40Mg 30 Film Coated Tablets '},
+  {'image':'./assets/images/best/hire-3.jpg',price:'$30','description':'Elbaluran 80Mg 30 Film Coated Tablets '},
+  {'image':'./assets/images/best/hire-4.jpg',price:'$50','description':'Cymbatex 20Mg 30 Capsules, Conventin 300Mg 30 Capsules '},
+  {'image':'./assets/images/best/hire-5.jpg',price:'$5','description':'Apilipex 30Mg 30 Tablets '},
+  {'image':'./assets/images/best/hire-6.jpg',price:'$4','description':'Sycocetam 500Mg 20 Tablets'},
+  {'image':'./assets/images/best/hire-7.jpg',price:'$15','description':'Citapronex 20Mg 14 Tablets '},
+  {'image':'./assets/images/best/hire-8.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+  {'image':'./assets/images/best/hire-9.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+  {'image':'./assets/images/best/hire-10.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+]
+// Health Care
+let Health_Care=[
+  {'image':'./assets/images/best/hea-1.jpg',price:'$20','description':'Jasper (Jl006) Lumbosacral Support - Xl '},
+  {'image':'./assets/images/best/hea-2.jpg',price:'$10','description':'Flyon (1702) Knee Support Elastic - 2Xl '},
+  {'image':'./assets/images/best/hea-3.jpg',price:'$30','description':'Dolphin Aerochamber L (5+ Years) '},
+  {'image':'./assets/images/best/hea-4.jpg',price:'$50','description':'Cymbatex 20Mg 30 Capsules, Conventin 300Mg 30 Capsules '},
+  {'image':'./assets/images/best/hea-5.jpg',price:'$5','description':'Fine Care Adult Diapers L(110-156Cm) 18P '},
+]
+
+// Hair Care
+let Beauty_Accessories=[
+  {'image':'./assets/images/best/be-1.jpg',price:'$20','description':'Nosta Age Eye Contour Soft Cream 20Ml '},
+  {'image':'./assets/images/best/be-2.jpg',price:'$10','description':'Biorganic Age-Defying Soft Eye Cream 30M '},
+  {'image':'./assets/images/best/be-3.jpg',price:'$30','description':'Akasia K Plus Eye&Face Cream 50Ml '},
+  {'image':'./assets/images/best/be-4.jpg',price:'$50','description':'Dahab All In One Lenses Solution 100Ml '},
+  {'image':'./assets/images/best/be-5.jpg',price:'$5','description':'Loreal Hyaluron Expert Anti-Aging Eye Cream 15Ml '},
+  {'image':'./assets/images/best/be-6.jpg',price:'$4','description':'Star Man Drompe Edt For Men 100Ml'},
+  {'image':'./assets/images/best/be-7.jpg',price:'$15','description':'Era Nail Polish Remover Lemon 200Ml '},
+  {'image':'./assets/images/best/be-8.jpg',price:'$5','description':'Era Nail Polish 13Ml # 166 '},
+  {'image':'./assets/images/best/be-9.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+  {'image':'./assets/images/best/be-10.jpg',price:'$5','description':'Cymbatex 20Mg 30 Capsules, Conventin 300Mg 30 Capsules'},
+]
+
+// Hair Care
+let best_Offers=[
+  {'image':'./assets/images/best/ofe-1.jpg',price:'$20','description':'Nosta Age Eye Contour Soft Cream 20Ml '},
+  {'image':'./assets/images/best/ofe-2.jpg',price:'$10','description':'Biorganic Age-Defying Soft Eye Cream 30M '},
+  {'image':'./assets/images/best/ofe-3.jpg',price:'$30','description':'Akasia K Plus Eye&Face Cream 50Ml '},
+  {'image':'./assets/images/best/ofe-4.jpg',price:'$50','description':'Dahab All In One Lenses Solution 100Ml '},
+  {'image':'./assets/images/best/ofe-5.jpg',price:'$5','description':'Loreal Hyaluron Expert Anti-Aging Eye Cream 15Ml '},
+  {'image':'./assets/images/best/ofe-6.jpg',price:'$4','description':'Star Man Drompe Edt For Men 100Ml'},
+  {'image':'./assets/images/best/ofe-7.jpg',price:'$15','description':'Era Nail Polish Remover Lemon 200Ml '},
+]
+
+let All_product=[
+  {'image':'./assets/images/best/be-1.jpg',price:'$20','description':'Nosta Age Eye Contour Soft Cream 20Ml '},
+  {'image':'./assets/images/best/be-2.jpg',price:'$10','description':'Biorganic Age-Defying Soft Eye Cream 30M '},
+  {'image':'./assets/images/best/be-3.jpg',price:'$30','description':'Akasia K Plus Eye&Face Cream 50Ml '},
+  {'image':'./assets/images/best/be-4.jpg',price:'$50','description':'Dahab All In One Lenses Solution 100Ml '},
+  {'image':'./assets/images/best/be-5.jpg',price:'$5','description':'Loreal Hyaluron Expert Anti-Aging Eye Cream 15Ml '},
+  {'image':'./assets/images/best/be-6.jpg',price:'$4','description':'Star Man Drompe Edt For Men 100Ml'},
+  {'image':'./assets/images/best/be-7.jpg',price:'$15','description':'Era Nail Polish Remover Lemon 200Ml '},
+  {'image':'./assets/images/best/be-8.jpg',price:'$5','description':'Era Nail Polish 13Ml # 166 '},
+  {'image':'./assets/images/best/be-9.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+  {'image':'./assets/images/best/be-10.jpg',price:'$5','description':'Cymbatex 20Mg 30 Capsules, Conventin 300Mg 30 Capsules'},
+  {'image':'./assets/images/best/ofe-1.jpg',price:'$20','description':'Nosta Age Eye Contour Soft Cream 20Ml '},
+  {'image':'./assets/images/best/ofe-2.jpg',price:'$10','description':'Biorganic Age-Defying Soft Eye Cream 30M '},
+  {'image':'./assets/images/best/ofe-3.jpg',price:'$30','description':'Akasia K Plus Eye&Face Cream 50Ml '},
+  {'image':'./assets/images/best/ofe-4.jpg',price:'$50','description':'Dahab All In One Lenses Solution 100Ml '},
+  {'image':'./assets/images/best/ofe-5.jpg',price:'$5','description':'Loreal Hyaluron Expert Anti-Aging Eye Cream 15Ml '},
+  {'image':'./assets/images/best/ofe-6.jpg',price:'$4','description':'Star Man Drompe Edt For Men 100Ml'},
+  {'image':'./assets/images/best/ofe-7.jpg',price:'$15','description':'Era Nail Polish Remover Lemon 200Ml '},
+  {'image':'./assets/images/best/1-t.jpg',price:'$20','description':'Elbaluran 60Mg 30 Film Coated Tablets Coated '},
+  {'image':'./assets/images/best/2-t.jpg',price:'$10','description':'Elbaluran 40Mg 30 Film Coated Tablets '},
+  {'image':'./assets/images/best/3-t.jpg',price:'$30','description':'Elbaluran 80Mg 30 Film Coated Tablets '},
+  {'image':'./assets/images/best/4-t.jpg',price:'$50','description':'Cymbatex 20Mg 30 Capsules, Conventin 300Mg 30 Capsules '},
+  {'image':'./assets/images/best/5-t.jpg',price:'$5','description':'Apilipex 30Mg 30 Tablets '},
+  {'image':'./assets/images/best/6-t.jpg',price:'$4','description':'Sycocetam 500Mg 20 Tablets'},
+  {'image':'./assets/images/best/7-t.jpg',price:'$15','description':'Citapronex 20Mg 14 Tablets '},
+  {'image':'./assets/images/best/8-t.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+  {'image':'./assets/images/best/hire-1.jpg',price:'$20','description':'Elbaluran 60Mg 30 Film Coated Tablets Coated '},
+  {'image':'./assets/images/best/hire-2.jpg',price:'$10','description':'Elbaluran 40Mg 30 Film Coated Tablets '},
+  {'image':'./assets/images/best/hire-3.jpg',price:'$30','description':'Elbaluran 80Mg 30 Film Coated Tablets '},
+  {'image':'./assets/images/best/hire-4.jpg',price:'$50','description':'Cymbatex 20Mg 30 Capsules, Conventin 300Mg 30 Capsules '},
+  {'image':'./assets/images/best/hire-5.jpg',price:'$5','description':'Apilipex 30Mg 30 Tablets '},
+  {'image':'./assets/images/best/hire-6.jpg',price:'$4','description':'Sycocetam 500Mg 20 Tablets'},
+  {'image':'./assets/images/best/hire-7.jpg',price:'$15','description':'Citapronex 20Mg 14 Tablets '},
+  {'image':'./assets/images/best/hire-8.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+  {'image':'./assets/images/best/hire-9.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+  {'image':'./assets/images/best/hire-10.jpg',price:'$5','description':'Imipramine 25Mg 50 Tablets '},
+  {'image':'./assets/images/best/hea-1.jpg',price:'$20','description':'Jasper (Jl006) Lumbosacral Support - Xl '},
+  {'image':'./assets/images/best/hea-2.jpg',price:'$10','description':'Flyon (1702) Knee Support Elastic - 2Xl '},
+  {'image':'./assets/images/best/hea-3.jpg',price:'$30','description':'Dolphin Aerochamber L (5+ Years) '},
+  {'image':'./assets/images/best/hea-4.jpg',price:'$50','description':'Cymbatex 20Mg 30 Capsules, Conventin 300Mg 30 Capsules '},
+  {'image':'./assets/images/best/hea-5.jpg',price:'$5','description':'Fine Care Adult Diapers L(110-156Cm) 18P '},
+]
+
+//******************************************************************************************************* */
+let product_grid = document.querySelector('.product-main .product-grid');
+const modal = document.querySelector('[data-modal]');
+// modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
+// mobile menu variables
+const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
+const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+  // mobile menu function
+  const mobileMenuCloseFunc = function () {
+    mobileMenu[i].classList.remove('active');
+    overlay.classList.remove('active');
+  }
+  mobileMenuOpenBtn[i].addEventListener('click', function () {
+    mobileMenu[i].classList.add('active');
+    overlay.classList.add('active');
+  });
+}
+// accordion variables
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+
+for (let i = 0; i < accordionBtn.length; i++) {
+
+  accordionBtn[i].addEventListener('click', function () {
+
+    const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+    for (let i = 0; i < accordion.length; i++) {
+
+      if (clickedBtn) break;
+
+      if (accordion[i].classList.contains('active')) {
+
+        accordion[i].classList.remove('active');
+        accordionBtn[i].classList.remove('active');
+
+      }
+
+    }
+
+    this.nextElementSibling.classList.toggle('active');
+    this.classList.toggle('active');
+
+  });
+
+}
+
+
+
+// I do this !---------------------------------------------------
+function getTimeDisccount() {
+  if(window.location.href=='http://127.0.0.1:5500/index.html'){
+    // this time stop of the interval
+  let dateinterval = new Date(2023, 12, 18, 3, 0, 0).getTime();
+  let day_ele = document.querySelectorAll(".countdown-box .countdown-content .display-Day")
+  let hours_ele = document.querySelectorAll(".countdown-box .countdown-content .display-hours")
+  let min_ele = document.querySelectorAll(".countdown-box .countdown-content .display-min")
+  let sec_ele = document.querySelectorAll(".countdown-box .countdown-content .display-sec")
+  let stopInterval = setInterval(() => {
+    // this time now
+    let dateTime = new Date().getTime()
+
+    // dif the time 
+    let datedif = dateinterval - dateTime;
+    let days = Math.floor(datedif / (1000 * 60 * 60 * 24));
+    // Display the remaining days
+    day_ele[0].innerHTML = days;
+    day_ele[1].innerHTML = days;
+    let hours = Math.floor((datedif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    hours_ele[0].innerHTML = hours
+    hours_ele[1].innerHTML = hours
+    let minutes = Math.floor((datedif % (1000 * 60 * 60)) / (1000 * 60))
+    min_ele[0].innerHTML = minutes < 10 ? `0${minutes}` : minutes
+    min_ele[1].innerHTML = minutes < 10 ? `0${minutes}` : minutes
+    let seconds = Math.floor((datedif % (1000 * 60)) / 1000)
+    sec_ele[0].innerHTML = seconds < 10 ? `0${seconds}` : seconds
+    sec_ele[1].innerHTML = seconds < 10 ? `0${seconds}` : seconds
+  })
+  }
+}
+//get catogary in link 
+function getQueryParamValue(paramName='catogary') {
+  // Get the current URL
+  var currentUrl = window.location.href;
+
+  // Create a URLSearchParams object from the URL
+  var urlParams = new URL(currentUrl);
+
+  // Get the value of the specified query parameter
+  var paramValue = urlParams.searchParams.get(paramName);
+
+  // Return the parameter value
+  return paramValue;
+}
+//select catogary with to display 
+function getCatogary(){
+let product=[]
+Catogary=getQueryParamValue();
+
+if(Catogary=='Medications'){
+  product=some_product
+}
+else if(Catogary=='Hair Care'){
+  product=Hair_Care
+}
+else if(Catogary=='Health Care'){
+  product=Health_Care
+}
+else if(Catogary=='Beauty Accessories'){
+  product=Beauty_Accessories
+}
+else if(Catogary=='best Offers'){
+  product=best_Offers
+}
+else{
+  if(window.location.href=='http://127.0.0.1:5500/index.html'){
+    product=some_product
+  }
+  else{
+    product=All_product
+  }
+
+}
+return product
+}
+//count the product in cart 
+function count_product_in_cart(){
+  let count=0
+  if("cart" in localStorage){
+    let getproduct=JSON.parse(localStorage.getItem('cart'));
+    count=getproduct.length;
+    document.querySelector("#count_bag").innerHTML=`${count}`
+   
+  }
+}
+// Define the addToCart function before it is referenced in the HTML
+function add (key,name_product){
+  let SetPoductCart=[]
+   choess_product=getCatogary();
+   let getProduct=choess_product[key];
+   
+    if("cart" in localStorage){
+      SetPoductCart=JSON.parse(localStorage.getItem('cart'));
+        let exist=SetPoductCart.find(item=>{
+        return item.description==getProduct.description && item.image==getProduct.image
+      })
+      if(!exist){
+        SetPoductCart.push(getProduct);
+        localStorage.setItem('cart',JSON.stringify(SetPoductCart));
+        count_product_in_cart();
+        setTimeout(()=>{
+          document.querySelector(".Notify_add").classList.remove('block');
+        },100)
+        setTimeout(()=>{
+          document.querySelector(".Notify_add").classList.add('block');
+        },3000)
+      }
+      else if(exist){
+        setTimeout(()=>{
+          document.querySelector(".Notify_in_cart").classList.remove('block');
+        },100)
+        setTimeout(()=>{
+          document.querySelector(".Notify_in_cart").classList.add('block');
+        },2000)
+      }
+    }
+
+    else{
+      SetPoductCart.push(getProduct);
+      localStorage.setItem('cart',JSON.stringify(SetPoductCart));
+      count_product_in_cart()
+      setTimeout(()=>{
+        document.querySelector(".Notify_add").classList.remove('block');
+        console.log(document.querySelector(".Notify_add"))
+      },100)
+      setTimeout(()=>{
+        document.querySelector(".Notify_add").classList.add('block');
+      },6000)
+    }
+  }
+  function searchProducts() {
+    // Get the search term from the input field
+    let searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    let products=getCatogary();
+      // Use regular expression to match the search term in the product descriptions
+    if(searchTerm!=''){
+      let regex = new RegExp(searchTerm, "g");
+      let searchResults = products.filter(function(product) {
+         return regex.test(product.description.toLocaleLowerCase());
+      });
+    // Display the search results
+    show_product(searchResults);
+    console.log(searchResults)
+    }
+  }
+
+  function RestProduct() {
+    // Get all elements with the id "Rest"
+    let all_div = document.querySelectorAll("#Rest");
+
+    // Check if there are any elements with the id "Rest"
+    if (all_div.length > 0) {
+        // Loop through each element and remove it
+        all_div.forEach(function (element) {
+            element.remove();
+        });
+    }
+}
+
+function show_product(searchProduct=null) {
+  let select_product=getCatogary();
+  if(searchProduct!=null){
+    select_product=searchProduct;
+    RestProduct();
+  }
+  select_product.forEach((value, key) => {
+    let cart = document.createElement('div');
+    cart.innerHTML = ` <div class="showcase" id="Rest">
+    <div class="showcase-banner ">
+      <img src=${value.image} alt="Mens Winter Leathers Jackets" width="300" class="product-img ">
+      <div class="showcase-actions">
+        <button class="btn-action">
+          <ion-icon name="heart-outline"></ion-icon>
+        </button>
+        <button class="btn-action">
+          <ion-icon name="add-outline" onclick="add(${key},'some_product')"></ion-icon>
+        </button>
+      </div>
+    </div>
+    <div class="showcase-content">
+      <a href="#">
+      <h3 class="showcase-title">${value.description}</h3>
+      </a>
+      <div class="showcase-rating">
+        <ion-icon name="star"></ion-icon>
+        <ion-icon name="star"></ion-icon>
+        <ion-icon name="star"></ion-icon>
+        <ion-icon name="star-outline"></ion-icon>
+        <ion-icon name="star-outline"></ion-icon>
+      </div>
+      <div class="price-box">
+        <p class="price">$48.00</p>
+        <del>$75.00</del>
+      </div>
+    </div>
+           </div>`
+    product_grid.appendChild(cart);
+
+  })
+}
+
+show_product();
+count_product_in_cart();
+getTimeDisccount();
+
+
+
+
